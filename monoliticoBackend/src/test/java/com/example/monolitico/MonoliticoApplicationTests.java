dc86055842e4bd8059801507b0971f1060c81ab2
Patricio Bustos <patricio.bustos.n@usach.cl>
2025-09-26 00:52:52 -0300

    proyecto doquerisado y con nginx, para varias instancias de backend y frontend, falta agregar el keycloak

--- monoliticoBackend/src/test/java/com/example/monolitico/MonoliticoApplicationTests.java
+++ monoliticoBackend/src/test/java/com/example/monolitico/MonoliticoApplicationTests.java
@@ -3,10 +3,10 @@ package com.example.monolitico;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class MonoliticoApplicationTests {
 
-	@Test
+//	@Test
 	void contextLoads() {
 	}
 
