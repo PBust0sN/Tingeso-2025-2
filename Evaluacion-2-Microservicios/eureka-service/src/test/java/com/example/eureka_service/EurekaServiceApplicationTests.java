76f24335cd4cb24c324583987e825b1803965b5e
Patricio Bustos <patricio.bustos.n@usach.cl>
2025-12-15 11:29:57 -0300

    eureka added

--- Evaluacion-2-Microservicios/eureka-service/src/test/java/com/example/eureka_service/EurekaServiceApplicationTests.java
+++ Evaluacion-2-Microservicios/eureka-service/src/test/java/com/example/eureka_service/EurekaServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.config_server;
+package com.example.eureka_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ConfigServerApplicationTests {
+class EurekaServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
