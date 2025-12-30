770eb9e884e0a7b280d3b43968759c891124b437
Patricio Bustos <patricio.bustos.n@usach.cl>
2025-12-30 00:52:38 -0300

    soy tonto

--- Evaluacion-2-Microservicios/microservicio-frontend/src/services/keycloak.js
+++ Evaluacion-2-Microservicios/microservicio-frontend/src/services/keycloak.js
@@ -1,31 +1,12 @@
 import Keycloak from "keycloak-js";
 
-// Obtiene la configuración de Keycloak desde la ventana global o usa valores por defecto
-const getKeycloakConfig = () => {
-  if (typeof window !== 'undefined' && window.KEYCLOAK_CONFIG) {
-    return window.KEYCLOAK_CONFIG;
-  }
-  
-  return {
-    url: "http://keycloak:8080",
-    realm: "toolRent",
-    clientId: "toolRent-Frontend"
-  };
-};
-
-const config = getKeycloakConfig();
-
 const keycloak = new Keycloak({
-  url: config.url,
-  realm: config.realm,
-  clientId: config.clientId,
+  url: "http://keycloak:8080",
+  realm: "toolRent",
+  clientId: "toolRent-Frontend",
 });
 
-console.log('Keycloak config:', config);
 console.log('Keycloak instance created:', keycloak);
-console.log('Keycloak URL:', keycloak.authServerUrl);
-console.log('Keycloak Realm:', keycloak.realm);
-console.log('Keycloak Client ID:', keycloak.clientId);
 
 
 export function initKeycloak(onAuthenticatedCallback) {
