c7959c1f174be3c8ab8e963b9aee2cdcf8544fd6
Patricio Bustos <patricio.bustos.n@usach.cl>
2026-01-05 22:19:34 -0300

    fix imagenes

--- Evaluacion-2-Microservicios/microservicio-frontend/src/services/images.service.js
+++ Evaluacion-2-Microservicios/microservicio-frontend/src/services/images.service.js
@@ -1,12 +1,10 @@
 import api from "../http-common";
 
-const uploadImage = (data, filename) => {
-    const params = new URLSearchParams();
-    if (filename) {
-        params.append('filename', filename);
-    }
+const uploadImage = (file, filename) => {
+    const formData = new FormData();
+    formData.append('file', file);
     
-    return api.post(`/images/upload${filename ? '?filename=' + filename : ''}`, data, {
+    return api.post(`/images/upload${filename ? '?filename=' + filename : ''}`, formData, {
         headers: {
             'Content-Type': 'multipart/form-data'
         }
