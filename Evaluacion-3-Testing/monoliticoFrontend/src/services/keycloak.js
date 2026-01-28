d6957a65133c4902eb4486521dff79a442acb1a5
Patricio Bustos <patricio.bustos.n@usach.cl>
2026-01-28 16:22:35 -0300

    cambios keycloak

--- Evaluacion-3-Testing/monoliticoFrontend/src/services/keycloak.js
+++ Evaluacion-3-Testing/monoliticoFrontend/src/services/keycloak.js
@@ -1,7 +1,7 @@
 import Keycloak from "keycloak-js";
 
 const keycloak = new Keycloak({
-  url: "http://localhost:8080",
+  url: "http://15.229.27.254/:8080",
   realm: "toolRent",
   clientId: "toolRent-Frontend",
 });
