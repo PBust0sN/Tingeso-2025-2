3b3dfc019b88c97eab76d932f4f142af8ec46f3b
Patricio Bustos <patricio.bustos.n@usach.cl>
2026-01-30 18:57:31 -0300

    avance

--- Evaluacion-3-Testing/monoliticoBackend/src/main/java/com/example/monolitico/config/SecurityConfig.java
+++ Evaluacion-3-Testing/monoliticoBackend/src/main/java/com/example/monolitico/config/SecurityConfig.java
@@ -59,7 +59,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("https://toolrent-tingeso.duckdns.org", "http://toolrent-tingeso.duckdns.org", "http://localhost:5173"));
         configuration.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
