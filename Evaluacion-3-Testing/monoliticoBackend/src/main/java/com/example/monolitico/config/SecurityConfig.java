585045b84210fd33828a3ef0ef55634e6bc252f1
Patricio Bustos <patricio.bustos.n@usach.cl>
2026-01-31 18:08:05 -0300

    avances

--- Evaluacion-3-Testing/monoliticoBackend/src/main/java/com/example/monolitico/config/SecurityConfig.java
+++ Evaluacion-3-Testing/monoliticoBackend/src/main/java/com/example/monolitico/config/SecurityConfig.java
@@ -2,7 +2,7 @@ package com.example.monolitico.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
