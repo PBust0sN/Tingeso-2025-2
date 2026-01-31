7568a8abf03e420f70031991931b3829f869680f
Patricio Bustos <patricio.bustos.n@usach.cl>
2026-01-31 00:35:33 -0300

    avance

--- Evaluacion-3-Testing/monoliticoBackend/src/main/java/com/example/monolitico/config/SecurityConfig.java
+++ Evaluacion-3-Testing/monoliticoBackend/src/main/java/com/example/monolitico/config/SecurityConfig.java
@@ -1,30 +1,15 @@
 package com.example.monolitico.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.JwtDecoders;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
-import java.util.Map;
 
 @Configuration
-@EnableMethodSecurity
 public class SecurityConfig {
-
 
     /**
      * CORS
