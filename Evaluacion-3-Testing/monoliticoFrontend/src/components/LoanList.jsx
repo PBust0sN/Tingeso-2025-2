50024762560745e2f9c862414abaa82e59095a58
Patricio Bustos <patricio.bustos.n@usach.cl>
2026-02-02 22:10:17 -0300

    correcion vista loan list

--- Evaluacion-3-Testing/monoliticoFrontend/src/components/LoanList.jsx
+++ Evaluacion-3-Testing/monoliticoFrontend/src/components/LoanList.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import loansService from "../services/loans.service";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
