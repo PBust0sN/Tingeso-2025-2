8bd6577da8296b82feecb27fadc641052a8e4856
Patricio Bustos <patricio.bustos.n@usach.cl>
2026-02-16 17:11:47 -0300

    fix de token en employee list

--- Evaluacion-3-Testing/monoliticoFrontend/src/components/EmployeeList.jsx
+++ Evaluacion-3-Testing/monoliticoFrontend/src/components/EmployeeList.jsx
@@ -33,7 +33,7 @@ const EmployeeList = () => {
 
   // Determine isAdmin from token payload
   const isAdmin = Boolean(
-     tokenPayload.realm_access?.roles?.includes("ADMIN")
+     tokenPayload?.realm_access?.roles === "ADMIN" || []
   );
 
   const filteredClient = client
